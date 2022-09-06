06bf67d1e7a227dd4820db1bc270299e7a1f95b3
sam gardner <samgardner20@gmail.com>
2022-09-06 11:49:37 -0400

    added in depth first traversal. one recursive implementation and one iterative

--- app.js
+++ app.js
@@ -44,6 +44,55 @@ class Graph {
         // 2.remove the now empty vertex completly from the adjacency list
         delete this.adjacencyList[vertex]
     }
+    // depth first traversal
+    DFTrecursive(start) {
+        // this is the results array we will be returning at the end
+        const results = []
+        // this will help keep track of where we have been
+        const visited = {}
+        // adding in this variable so the context of this can be used in the lower function
+        const adjacencyList = this.adjacencyList
+        // this is our recursive function that does the bulk of the work
+        function DFS(vertex){
+            // base case
+            if(!vertex) return null
+            // set visted to true for the given vertex
+            visited[vertex] = true
+            results.push(vertex)
+            // for each edge(neighbor), see if we have visted. if not, call again on that neighbor
+            adjacencyList[vertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                   return DFS(neighbor)
+                }
+            });
+        }
+        DFS(start)
+        return results
+    }
+    // these will return slightly different results but are both Depth first implementations
+    DFTiterative(start) {
+        let results = []
+        // make a stack from an array, instead of using the call stack like in the recursive implementation
+        let stack = []
+        let visited = {}
+        // defining this here so we dont have to redefine it every time in the while loop
+        let currentVertex
+        stack.push(start)
+        while(stack.length > 0){
+            currentVertex = stack.pop()
+            if(!visited[currentVertex]){
+                visited[currentVertex] = true
+                results.push(currentVertex)
+                // loop through the current vertex's neighbors and add them to the stack
+                this.adjacencyList[currentVertex].forEach(neighbor => {
+                    if(!visited[neighbor]){
+                        stack.push(neighbor)
+                    }
+                })
+            }
+        }
+        return results
+    }
 }
 
 let g = new Graph
@@ -58,6 +107,6 @@ g.addEdge("Austin", "Cooperstown")
 
 
 
-
-console.log(g);
+console.log(g.adjacencyList);
+console.log(g.DFTiterative("Austin"));
 
